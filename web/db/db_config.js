23e7c70de51c2781fcd7a287173318672451d9bc
zfreedman <zfreedman92@gmail.com>
2015-10-15 11:51:18 -0700

    Update on Server_zf - Added db_config.js file for Sequelize scaffolding - Included bcrypt as a dependency in package.json

---
+++ web/db/db_config.js
@@ -0,0 +1,25 @@
+// Note: comments done with
+// http://patorjk.com/software/taag/#p=display&f=Doom&t=Logger
+
+//                       _              _ 
+//                      (_)            | |
+//  _ __ ___  __ _ _   _ _ _ __ ___  __| |
+// | '__/ _ \/ _` | | | | | '__/ _ \/ _` |
+// | | |  __/ (_| | |_| | | | |  __/ (_| |
+// |_|  \___|\__, |\__,_|_|_|  \___|\__,_|
+//              | |                       
+//              |_|       
+
+var Sequelize = require('sequelize');
+
+//           _               
+//          | |              
+//  ___  ___| |_ _   _ _ __  
+// / __|/ _ \ __| | | | '_ \ 
+// \__ \  __/ |_| |_| | |_) |
+// |___/\___|\__|\__,_| .__/ 
+//                    | |    
+//                    |_|    
+
+// Setup sequelize to connect to InsultPvP
+var sequelize = new Sequelize('');
