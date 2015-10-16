dfeafc067986bd20b9933127d7359411c6a48979
bkillenit <blaine.j.killen@gmail.com>
2015-10-16 15:09:19 -0700

    matching now returns pairs of matches from batch

--- web/lib/matchingAlgorithm.js
+++ web/lib/matchingAlgorithm.js
@@ -1,17 +1,14 @@
 // Table: PlayerData
 // Variable Names:
 // winLoss: Float
-// type: Post MVP
+// type: string (Post MVP)
 // winVelocity: Float
 // rank: String
 // winStreak: Integer
 
-var playerObj = {winLoss: 1.2, winVelocity: 3, rank: 4, winStreak: 10};
-var features = Object.keys(playerObj);
-
 // initializing the coefficient values to multiply eaxh x by,
 // this gives weighting to the x1, x2 ... xn
-var thetas = {winLoss: 1, winVelocity: 1.2, rank: 1.8, winStreak: .7};
+var thetas = {winLossRatio: 1, winVelocity: 1.2, rank: 1.5, winStreak: .7};
 
 // not a deep clone
 var cloneObj = function(obj) {
@@ -25,7 +22,7 @@ var cloneObj = function(obj) {
 }
 
 // finding the best opponent for a player to play
-var findOpponent = function() {
+var findOpponent = function(playerObj, otherPlayers, features) {
   var bestMatch = [Infinity, null];
 
   // initializing min and max with the player stats
@@ -36,55 +33,54 @@ var findOpponent = function() {
   // and exert the same influence on the overall algorithm
 
   // find the min and max for each feature in dataset
-  otherPlayers.forEach( function(player, index) {
+  Object.keys(otherPlayers).forEach( function(name) {
+    var player = otherPlayers[name];
+
     // if the users searching for matchmakin is not within a one rank margin,
     // dont consider the user as a potential match
     if(Math.abs(player.rank - playerObj.rank) <= 1) {
       features.forEach( function(feature) {
-        if(feature !== 'username') {
-          var val = player[feature];
+        var val = player[feature];
 
-          if( val > max[feature] ) {
-            max[feature] = val;
-          }
-          if( val < min[feature] ) {
-            min[feature] = val;
-          }
+        if( val > max[feature] ) {
+          max[feature] = val;
+        }
+        else if( val < min[feature] ) {
+          min[feature] = val;
         }
       });
     } else {
-      delete otherPlayers[index];
+      delete otherPlayers[name];
     }
-  })
+  });
 
   // scale the players stats
   for(feature in playerObj) {
-    if(feature !== 'username') {
-      var val = playerObj[feature];
-      playerObj[feature] = (val - min[feature]) / (max[feature] - min[feature]);
-    }
+    var val = playerObj[feature];
+    playerObj[feature] = (val - min[feature]) / (max[feature] - min[feature]);
   }
 
   // perform feature scaling using the rescaling equation, scales each value from 0 to 1 relative to other data
   // points of same feature from all other potential matches.
-  otherPlayers.forEach( function(player) {
-    features.forEach( function(feature) {
-      if(feature !== 'username') {
-        var val = player[feature];
+  Object.keys(otherPlayers).forEach( function(name) {
+    var player = otherPlayers[name];
 
-        player[feature] = (val - min[feature]) / (max[feature] - min[feature]);
-      }
+    features.forEach( function(feature) {
+      var val = player[feature];
+
+      player[feature] = (val - min[feature]) / (max[feature] - min[feature]);
     });
   });
   // end of feature scaling
 
   // find the least euclidean distance of players searching for a match
   // to see which player most closely matches current player searching
-  otherPlayers.forEach( function(player) {
-    var distance = Math.sqrt(Math.pow((player.winLoss - playerObj.winLoss) * thetas['winLoss'], 2) + Math.pow((player.winVelocity - playerObj.winVelocity) * thetas['winVelocity'], 2) + Math.pow((player.rank - playerObj.rank) * thetas['rank'], 2) + Math.pow((player.winStreak - playerObj.winStreak) * thetas['winStreak'], 2));
+  Object.keys(otherPlayers).forEach( function(name) {
+    var player = otherPlayers[name];
+    var distance = Math.sqrt(Math.pow((player.winLossRatio - playerObj.winLossRatio) * thetas['winLossRatio'], 2) + Math.pow((player.winVelocity - playerObj.winVelocity) * thetas['winVelocity'], 2) + Math.pow((player.rank - playerObj.rank) * thetas['rank'], 2) + Math.pow((player.winStreak - playerObj.winStreak) * thetas['winStreak'], 2));
 
     if(distance < bestMatch[0]) {
-      bestMatch = [distance, player];
+      bestMatch = [distance, name];
     }
   });
 
