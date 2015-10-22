4dce5833792602be1459a6213f59742bac64816e
Bowen Yang <carlyang007@gmail.com>
2015-10-21 19:15:35 -0700

    Working on auth token

--- public/auth/signup.js
+++ public/auth/signup.js
@@ -1,6 +1,6 @@
 angular.module('VBattle.signup', [])
 // Sign up controller
-.controller('SignupCtrl', function ($rootScope, $scope, $location, Auth) {
+.controller('SignupCtrl', function ($rootScope, $scope, $location, $auth) {
   $scope.signup = function () {
     var user = {
       username: $scope.username,
@@ -8,15 +8,11 @@ angular.module('VBattle.signup', [])
       email: $scope.email
     };
     // Post username and password to sign up; receives JSON object with signupSuccess and usernameAvailable as booleans upon failure, and data about user upon success 
-    Auth.signup(user)
+    $auth.signup(user)
     .then(function (data) {
       console.log(data);
       $scope.username = '';
       $scope.password = '';
-      if (data.signupSuccess) {
-        $rootScope.auth = true;
-        $location.path('/');
-      }
     });
   };
 });
