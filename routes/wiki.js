63e324011245b066b462caa2e42f896f038590ab
bkillenit <blaine.j.killen@gmail.com>
2015-10-21 19:03:45 -0700

    started content pasrsing

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,23 +1,23 @@
 // Note: comments done with
 // http://patorjk.com/software/taag/#p=display&f=Doom&t=Logger
 
-//                       _              _ 
+//                       _              _
 //                      (_)            | |
 //  _ __ ___  __ _ _   _ _ _ __ ___  __| |
 // | '__/ _ \/ _` | | | | | '__/ _ \/ _` |
 // | | |  __/ (_| | |_| | | | |  __/ (_| |
 // |_|  \___|\__, |\__,_|_|_|  \___|\__,_|
-//              | |                       
-//              |_|                         
+//              | |
+//              |_|
 
 // Requirements
 var express = require('express');
 var router = express.Router();
 var wikiHelper = require('../lib/wikiHelper');
 
-//                  _            
-//                 | |           
-//  _ __ ___  _   _| |_ ___  ___ 
+//                  _
+//                 | |
+//  _ __ ___  _   _| |_ ___  ___
 // | '__/ _ \| | | | __/ _ \/ __|
 // | | | (_) | |_| | ||  __/\__ \
 // |_|  \___/ \__,_|\__\___||___/
@@ -31,7 +31,13 @@ router.get('/:avatarName', function (req, res, next) {
     avatarName: req.params.avatarName,
     // Callback
     callback: function (result) {
-      res.send(result);
+      // filter out profile snippets from result
+      var content = result.query.pages[0].revisions[0].content;
+
+      // parse content from sent to us from wikipedia
+      wikiHelper.parseContent(content);
+
+      res.send(content);
     }
   };
 
