6e711fc8e4848253e550d6a4fbad32be30e54695
simon <schmalfuss.simon@gmail.com>
2015-10-23 16:01:51 -0700

    updated voting controller functionality

--- public/voting/voting.js
+++ public/voting/voting.js
@@ -5,9 +5,25 @@ angular.module('VBattle.voting', [])
 
   //makes get request to next rooms of queue and store them in the storage
   //
-  localStorage.setItem("itworks", true);
+ $scope.showMessages = 1; 
+
+ $scope.messages = [{sender:"simon", message:"why dont you bowen install?"}, {sender:"bowen", message:"get back eating your cake"}];
+ $scope.newMessages = [{sender:"peter", message:"fuck it"}];
+
+ $scope.users = ["simon", "bowen"];
   
-  
+  //socket.emit() when need for new chats
+  //socket.on() for getting latest chats -> storing them in the localstorage
+  // and getting rid of them after user voted
+  $scope.upvote = function (input) {
+  	console.log("upvoting for", input);
+  	//getting next messages object from local storage and removing the old one
+  	$scope.messages = $scope.newMessages;
+  	
+  	$scope.users = ["peter"];
+
+  };
+
 
 
 
