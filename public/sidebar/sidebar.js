b8acdd6342ad5d7e77f57ed5cfe1cb17771cdd3b
simon <schmalfuss.simon@gmail.com>
2015-10-27 20:18:55 -0700

    fixed indention errors

--- public/sidebar/sidebar.js
+++ public/sidebar/sidebar.js
@@ -1,6 +1,20 @@
 angular.module('VBattle.sideBar', [])
 
-.controller('SidebarCtrl', function ($auth, $scope, $location, $element) {
+.controller('SidebarCtrl', function ($auth, $scope, $location, $element, socketFactory) {
+  if ($auth.isAuthenticated()) {
+    var mySocket = socketFactory();
+    mySocket.emit('client:linkUser', {
+      token: window.localStorage['satellizer_token']
+    });                                                             
+    
+    mySocket.on('client:joinRoom', function (data) {
+      console.log("join-room update", data);
+    });
+  
+    mySocket.on('client:turnUpdate', function (data) {
+      console.log("new message update", data);
+    });
+  }
 
   // indicate whether sidebar is visible or not
   $scope.state = false;
