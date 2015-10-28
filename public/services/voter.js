8fa2961a93bc9603f70d230171ef5afefd3ba22c
Bowen Yang <carlyang007@gmail.com>
2015-10-28 12:23:51 -0700

    fixed linter error

--- public/services/voter.js
+++ public/services/voter.js
@@ -1,39 +1,32 @@
 angular.module('VBattle.votingServices', [])
 .factory('Voter', function ($http) {
 
- var vote = function (roomsID, avatarID) {
-  return $http.put('/voting/' + roomsID, {winner: avatarID})
-    .then(function (result) {
-      return result.data;
-    }, function (err) {
-      throw err;
-    });
+  var vote = function (roomsID, avatarID) {
+    return $http.put('/voting/' + roomsID, { winner: avatarID })
+      .then(function (result) {
+        return result.data;
+      }, function (err) {
+        throw err;
+      });
+  };
 
-};
+  var getRooms = function () {
+    return $http.get('/judging');
+  };
 
-var getRooms = function () {
-  return $http.get('/judging');
+  var updateStats = function (obj) {
+    return $http.put("/judging/" + obj.roomID, { upVoteID: obj.avatarID })
+      .then( function (result) {
+        console.log("updated room");
+      })
+      .catch(function (err) {
+        console.error("error happened", err);
+      });
+  };
 
-};
-
-var updateStats = function (obj) {
-
-  return $http.put("/judging/"+obj.roomID, {upVoteID: obj.avatarID})
-  .then( function (result) {
-    console.log("updated room");
-
-  })
-  .catch( function (err) {
-    console.error("error happened", err);
-  })
-
-
-};
-
-
- return {
-  vote: vote,
-  getRooms: getRooms,
-  updateStats: updateStats
-    };
+  return {
+    vote: vote,
+    getRooms: getRooms,
+    updateStats: updateStats
+  };
 });
--- public/voting/voting.js
+++ public/voting/voting.js
@@ -1,19 +1,13 @@
 angular.module('VBattle.voting', [])
 
 .controller('VotingCtrl', function ($scope, $rootScope, $location, Profile, Voter) {
- 
-
   //makes get request to next rooms of queue and store them in the storage
-  //
- $scope.showMessages = 1; 
- $scope.avatarOne; 
- $scope.avatarTwo;
-
-  
+  $scope.showMessages = 1; 
+  $scope.avatarOne; 
+  $scope.avatarTwo;
   //socket.emit() when need for new chats
   //socket.on() for getting latest chats -> storing them in the localstorage
   //and getting rid of them after user voted
-  //zack
   $scope.upvote = function (input) {
     console.log("upvoting for", input);
     //getting next messages object from local storage and removing the old one
@@ -24,15 +18,13 @@ angular.module('VBattle.voting', [])
     //judging route
     var rooms = window.localStorage["voteRooms"] || {};
     Voter.getRooms().then( function (result) {
-    console.log(result.data, "resultObj");
-    window.localStorage["voteRooms"] = JSON.stringify(result.data);  
-    //$scope.getNext();
-
-    }) 
+      console.log(result.data, "resultObj");
+      window.localStorage["voteRooms"] = JSON.stringify(result.data);  
+      //$scope.getNext();
+    })
     .catch(function (err) {
       console.error(err);
-    })
-
+    });
   };
 
   $scope.getNext = function (userID) {
@@ -45,32 +37,24 @@ angular.module('VBattle.voting', [])
     Voter.updateStats(obj);
     //make post request to room update -> userId as an put request body
     if (window.localStorage["voteRooms"] !== "[]") {
-    var rooms = JSON.parse(window.localStorage["voteRooms"]);
-    console.log("parsed rooms", rooms);
-    var room = rooms[Object.keys(rooms)[0]];
-    $scope.avatarOne = room.avatar2.avatarID;
-    $scope.avatarTwo = room.avatar1.avatarID;
-    console.log("next room", room);
-
-    if (room) {
-      console.log(rooms, "out of local storage");
-      $scope.messages = room.messages;
-
-      //deleting the first room object so when another interval gets run the
-      //user will receive the next chatroom
-      
-      delete rooms[Object.keys(rooms)[0]];
-      console.log(rooms, "back to local storage");
-      window.localStorage["voteRooms"] = rooms;
-    } else {
-      $scope.messages = [];
+      var rooms = JSON.parse(window.localStorage["voteRooms"]);
+      console.log("parsed rooms", rooms);
+      var room = rooms[Object.keys(rooms)[0]];
+      $scope.avatarOne = room.avatar2.avatarID;
+      $scope.avatarTwo = room.avatar1.avatarID;
+      console.log("next room", room);
+      if (room) {
+        console.log(rooms, "out of local storage");
+        $scope.messages = room.messages;
+        //deleting the first room object so when another interval gets run
+        //user will receive the next chatroom 
+        delete rooms[Object.keys(rooms)[0]];
+        console.log(rooms, "back to local storage");
+        window.localStorage["voteRooms"] = rooms;
+      } else {
+        $scope.messages = [];
+      }
     }
-  }
-
   };
-
   $scope.getVotes();
-  
-
-  
 });
