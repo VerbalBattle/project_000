9b8841453b68917ed17c01d00bdb19b9949de776
zfreedman <zfreedman92@gmail.com>
2015-10-31 12:20:48 -0700

    Update on Frontend_zf - Began implementing dynamic resizing using jQuery based on window     window size (on top of already implemented Bootstrap) - Added title divs to lobby avatars view

---
+++ public/styleScipts/resizeCSS.js
@@ -0,0 +1,31 @@
+// On document ready
+$(document).ready(function () {
+  // Window resizing
+  $(window).resize(function () {
+
+    //  _           _     _            ______          _         
+    // | |         | |   | |           | ___ \        (_)        
+    // | |     ___ | |__ | |__  _   _  | |_/ /___  ___ _ _______ 
+    // | |    / _ \| '_ \| '_ \| | | | |    // _ \/ __| |_  / _ \
+    // | |___| (_) | |_) | |_) | |_| | | |\ \  __/\__ \ |/ /  __/
+    // \_____/\___/|_.__/|_.__/ \__, | \_| \_\___||___/_/___\___|
+    //                           __/ |                           
+    //                          |___/      
+
+    // Room summary recalculations
+
+    // Opponent image width in px
+    var opponentImageWidth = $('.lobbyView_opponentImage').width();
+    // Room container width in px
+    var roomContainerWidth = $('.lobbyView_roomContainer').width();
+    // Desired of room summary
+    var roomSummaryPercentage =
+      (1 - opponentImageWidth/roomContainerWidth) * 100 + '%';
+    // Get opponent image width
+    $('.lobbyView_roomSummary').css({
+      'width': roomSummaryPercentage
+    });
+
+
+  });
+});
