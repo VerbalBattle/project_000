4dada4ded445a782bd49f4169445e1e3a92167ff
bkillenit <blaine.j.killen@gmail.com>
2015-11-05 11:29:24 -0800

    changed port and host for pgsql

--- db/db_config.js
+++ db/db_config.js
@@ -28,8 +28,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    port: match[4],
-    host: match[3],
+    port: 5432,
+    host: 'ec2-107-21-222-62.compute-1.amazonaws.com',
     logging: true //false
   });
 } else {
