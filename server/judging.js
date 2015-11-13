bddd5d10d38c44bf94ae61283a5f90af015c8309
Bowen Yang <carlyang007@gmail.com>
2015-11-13 00:52:09 -0800

    added product landing images

--- server/judging.js
+++ server/judging.js
@@ -36,7 +36,7 @@ judging.roomDataForServer = {};
 judging.roomIDsByExpiration = [];
 
 // Judging initial time to expire
-judging.initialTTE = 30000;
+judging.initialTTE = 300000;
 
 // Judging function to add a new room to be judged
 judging.addRoom = function (roomID, roomDataFinder) {
